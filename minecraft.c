a07d86b8129b45667aa0c43c2fbd24c757ccae5d
Gwalker1313 <111939848+Gwalker1313@users.noreply.github.com>
2025-06-03 10:04:33 -0600

    Terminal initialization and restoration.

--- minecraft.c
+++ minecraft.c
@@ -1,9 +1,44 @@
+#include <fcntl.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
-#include <fcntl.h>
 
-int main() {
+// Function headers
+void init_terminal();
+void restore_terminal();
+void handle_input();
+
+static struct termios org_term_state, new_term_state;
+
+void init_terminal()
+{
+    tcgetattr(STDIN_FILENO, &org_term_state);
+    new_term_state = org_term_state;
+    new_term_state.c_lflag &= ~(ICANON | ECHO);        // Disable flags canonical mode and echo for non-blocking input
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_term_state); // Enable flag to apply changes immediately
+    fflush(stdout);
+}
+
+void restore_terminal()
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &org_term_state);
+    fflush(stdout);
+    printf("Terminal restored\n");
+}
+
+void handle_input() {}
+
+int main()
+{
+    init_terminal();
+
+    // Game Loop
+    // while (1)
+    // {
+    //     handle_input();
+    // }
+
+    restore_terminal();
     return 0;
 }
